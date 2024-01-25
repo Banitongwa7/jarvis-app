903d641eda73651fa2f05f9a155eec308d0eaa30
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-25 18:42:50 +0100

    copy 1 file and update 2 files

---
+++ src/components/Features.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Features() {
+  return (
+    <View>
+      <Text>features</Text>
+    </View>
+  )
+}
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
 
 export default function HomeScreen() {
+  const [msg, setMsg] = useState([]);
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View className="flex-1 bg-white">
+      <SafeAreaView className="flex-1 flex mx-5">
+      <View className="flex-row justify-center">
+        <Image source={require("./../../assets/images/bot.png")} style={{ width: wp(15), height: hp(15) }} />
+      </View>
+
+
+      </SafeAreaView>
     </View>
   )
 }
