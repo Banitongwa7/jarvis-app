dfe1d29bbb35e763d25279eebbb307df7902f217
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-25 17:32:42 +0100

    update 1 file and create 8 files

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-cyan-800">
+    <SafeAreaView className="flex-1 items-center justify-center bg-cyan-800">
       <Text className="text-white text-3xl font-bold">Hello friend !</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
