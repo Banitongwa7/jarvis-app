3b9023d6a58b336e185d6b2a48d1cd5da63c64f7
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-25 18:13:08 +0100

    navigation

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text } from 'react-native';
+import AppNavigation from './src/navigation/index'
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-cyan-800">
-      <Text className="text-white text-3xl font-bold">Hello friend !</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <AppNavigation />
   );
 }
