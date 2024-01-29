fa66c94a96728bc43e46ed235fa76c82902b0e8d
David BANITONGWA <davidbanitongwa@gmail.com>
2024-01-29 16:34:20 +0100

    update 2 files and create 1 file

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,10 +1,84 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function Features() {
   return (
-    <View>
-      <Text>features</Text>
+    <View style={{ height: hp(60) }} className="space-y-4">
+      <Text
+        style={{ fontSize: wp(6.5) }}
+        className="font-semibold text-gray-700"
+      >
+        features
+      </Text>
+      <View className="bg-emerald-200 p-4 rounded-xl space-x-1">
+        <View className="flex-row space-x-1 items-center">
+          <Image
+            source={require("./../../assets/images/chatgptIcon.png")}
+            style={{ width: wp(4), height: hp(4) }}
+          />
+          <Text
+            style={{ fontSize: wp(4.8) }}
+            className="font-semibold text-gray-700"
+          >
+            ChatGPT
+          </Text>
+        </View>
+        <Text
+          style={{ fontSize: wp(3.8) }}
+          className="text-gray-700 font-medium"
+        >
+          ChatGPT can provide you with instant and knowledgeable responses,
+          assist you with creative ideas on a wide rangeof topics.
+        </Text>
+      </View>
+
+      <View className="bg-purple-200 p-4 rounded-xl space-x-1">
+        <View className="flex-row space-x-1 items-center">
+          <Image
+            source={require("./../../assets/images/dalleIcon.png")}
+            style={{ width: wp(4), height: hp(4) }}
+          />
+          <Text
+            style={{ fontSize: wp(4.8) }}
+            className="font-semibold text-gray-700"
+          >
+            DALL-E
+          </Text>
+        </View>
+        <Text
+          style={{ fontSize: wp(3.8) }}
+          className="text-gray-700 font-medium"
+        >
+          DALL-E can generate imaginative and diverse images from textual
+          descriptions, expanding the boundaries of visual creativity.
+        </Text>
+      </View>
+
+      <View className="bg-cyan-200 p-4 rounded-xl space-x-1">
+        <View className="flex-row space-x-1 items-center">
+          <Image
+            source={require("./../../assets/images/smartaiIcon.png")}
+            style={{ width: wp(4), height: hp(4) }}
+          />
+          <Text
+            style={{ fontSize: wp(4.8) }}
+            className="font-semibold text-gray-700"
+          >
+            Smart AI
+          </Text>
+        </View>
+        <Text
+          style={{ fontSize: wp(3.8) }}
+          className="text-gray-700 font-medium"
+        >
+          A powerful voice assistant with the abilities of ChatGPT and DALL-E,
+          providing you the best of both worlds.
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,80 @@
-import { View, Text, Image, SafeAreaView } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, Image, SafeAreaView, ScrollView } from "react-native";
+import React, { useState } from "react";
+import Features from "./../components/Features";
+import { dummyMessages } from "../utils";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default function HomeScreen() {
-  const [msg, setMsg] = useState([]);
+  const [msg, setMsg] = useState(dummyMessages);
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex-1 flex mx-5">
-      <View className="flex-row justify-center">
-        <Image source={require("./../../assets/images/bot.png")} style={{ width: wp(15), height: hp(15) }} />
-      </View>
+        <View className="flex-row justify-center">
+          <Image
+            source={require("./../../assets/images/bot.png")}
+            style={{ width: wp(15), height: hp(15) }}
+          />
+        </View>
 
-
+        {msg.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{ fontSize: wp(5) }}
+              className="text-gray-700 font-semibold ml-1"
+            >
+              Assistant
+            </Text>
+            <View
+              style={{ height: hp(58) }}
+              className="bg-neutral-200 rounded-3xl p-4"
+            >
+              <ScrollView
+                bounces={false}
+                className="space-y-4"
+                showsVerticalScrollIndicator={false}
+              >
+                {msg.map((item, index) =>
+                  item.role == "assistant" ? (
+                    item.content.includes("https") ? (
+                      <View key={index} className="flex-row justify-start">
+                        <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                          <Image
+                            source={{ uri: item.content }}
+                            className="rounded-2xl"
+                            style={{ width: wp(60), height: hp(60) }}
+                          />
+                        </View>
+                      </View>
+                    ) : (
+                      <View
+                        key={index}
+                        style={{ width: wp(70) }}
+                        className="bg-emerald-100 rounded-xl p-2 rounded-tl-none"
+                      >
+                        <Text>{item.content}</Text>
+                      </View>
+                    )
+                  ) : (
+                    <View key={index} className="flex-row justify-end">
+                      <View
+                        style={{ width: wp(70) }}
+                        className="bg-white rounded-xl p-2 rounded-tr-none"
+                      >
+                        <Text>{item.content}</Text>
+                      </View>
+                    </View>
+                  )
+                )}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
       </SafeAreaView>
     </View>
-  )
+  );
 }
